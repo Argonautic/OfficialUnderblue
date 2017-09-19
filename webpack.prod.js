476d69a3a9a6c8892b5313a6328097e5acd39815
Jon <jonathan.zhou93@gmail.com>
2017-09-19 16:00:38 -0400

    for heroku

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,8 +2,6 @@ const merge = require('webpack-merge');
 const webpack = require('webpack');
 const common = require('./webpack.config.js');
 
-console.log('hi');
-
 module.exports = merge(common, {
     plugins: [
         new webpack.DefinePlugin({
